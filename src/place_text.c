c0b77cf83db58f792bd9f1d6f0f3e87151e7225b
siliconspecter <siliconspecter@outlook.com>
2025-06-08 22:51:56 +0100

    Fix misalignment of single lines.

--- src/place_text.c
+++ src/place_text.c
@@ -1,32 +1,43 @@
+#include "place_text.h"
+#include "placed_text.h"
 #include "text_font.h"
 #include "written_text.h"
-#include "placed_text.h"
-#include "place_text.h"
 
-static int end_line(
-    const int horizontal_alignment,
-    placed_text *const placed_text,
-    const int height_of_previous_line,
-    const int height_of_current_line,
-    const int line_spacing_of_previous_line,
-    const int line_spacing_of_current_line,
-    const int width_of_current_line,
-    const int index_of_first_glyph_on_current_line,
-    const int number_of_banked_lines)
-{
-  const int output = height_of_current_line + (line_spacing_of_previous_line > line_spacing_of_current_line ? line_spacing_of_previous_line : line_spacing_of_current_line) * number_of_banked_lines + (height_of_previous_line > height_of_current_line ? height_of_previous_line : height_of_current_line) * (index_of_first_glyph_on_current_line == 0 ? number_of_banked_lines : number_of_banked_lines - 1);
+static int end_line(const int horizontal_alignment,
+                    placed_text *const placed_text,
+                    const int height_of_previous_line,
+                    const int height_of_current_line,
+                    const int line_spacing_of_previous_line,
+                    const int line_spacing_of_current_line,
+                    const int width_of_current_line,
+                    const int index_of_first_glyph_on_current_line,
+                    const int number_of_banked_lines) {
+  const int output =
+      height_of_current_line +
+      (line_spacing_of_previous_line > line_spacing_of_current_line
+           ? line_spacing_of_previous_line
+           : line_spacing_of_current_line) *
+          number_of_banked_lines +
+      (height_of_previous_line > height_of_current_line
+           ? height_of_previous_line
+           : height_of_current_line) *
+          (index_of_first_glyph_on_current_line == 0
+               ? number_of_banked_lines
+               : number_of_banked_lines - 1);
 
-  for (int glyph_index = index_of_first_glyph_on_current_line; glyph_index < placed_text->number_of_placed_glyphs; glyph_index++)
-  {
-    placed_text->placed_glyph_rows[glyph_index] += output - placed_text->placed_text_fonts[glyph_index]->line_height;
+  for (int glyph_index = index_of_first_glyph_on_current_line;
+       glyph_index < placed_text->number_of_placed_glyphs; glyph_index++) {
+    placed_text->placed_glyph_rows[glyph_index] +=
+        output - placed_text->placed_text_fonts[glyph_index]->line_height;
   }
 
-  if (horizontal_alignment != RIGHT_OF_TEXT_ANCHOR)
-  {
-    const int amount_to_shift = horizontal_alignment == CENTERED_ON_TEXT_ANCHOR ? width_of_current_line / 2 : width_of_current_line;
+  if (horizontal_alignment != RIGHT_OF_TEXT_ANCHOR) {
+    const int amount_to_shift = horizontal_alignment == CENTERED_ON_TEXT_ANCHOR
+                                    ? width_of_current_line / 2
+                                    : width_of_current_line;
 
-    for (int glyph_index = index_of_first_glyph_on_current_line; glyph_index < placed_text->number_of_placed_glyphs; glyph_index++)
-    {
+    for (int glyph_index = index_of_first_glyph_on_current_line;
+         glyph_index < placed_text->number_of_placed_glyphs; glyph_index++) {
       placed_text->placed_glyph_columns[glyph_index] -= amount_to_shift;
     }
   }
@@ -34,14 +45,9 @@ static int end_line(
   return output;
 }
 
-void place_text(
-    const written_text written_text,
-    const int vertical_alignment,
-    const int horizontal_alignment,
-    const int row,
-    const int column,
-    placed_text *const placed_text)
-{
+void place_text(const written_text written_text, const int vertical_alignment,
+                const int horizontal_alignment, const int row, const int column,
+                placed_text *const placed_text) {
   int total_height = 0;
   int height_of_previous_line = 0;
   int height_of_current_line = 0;
@@ -52,19 +58,22 @@ void place_text(
   int index_of_first_glyph_on_current_line = 0;
   int number_of_banked_lines = 0;
 
-  for (int code_point_index = 0; code_point_index < written_text.number_of_written_code_points; code_point_index++)
-  {
-    const int glyph_index = written_text.written_glyph_indices[code_point_index];
+  for (int code_point_index = 0;
+       code_point_index < written_text.number_of_written_code_points;
+       code_point_index++) {
+    const int glyph_index =
+        written_text.written_glyph_indices[code_point_index];
 
-    if (glyph_index == -1)
-    {
-      if (index_of_first_glyph_on_current_line == placed_text->number_of_placed_glyphs)
-      {
+    if (glyph_index == -1) {
+      if (index_of_first_glyph_on_current_line ==
+          placed_text->number_of_placed_glyphs) {
         number_of_banked_lines++;
-      }
-      else
-      {
-        total_height += end_line(horizontal_alignment, placed_text, height_of_previous_line, height_of_current_line, line_spacing_of_previous_line, line_spacing_of_current_line, width_of_current_line, index_of_first_glyph_on_current_line, number_of_banked_lines);
+      } else {
+        total_height += end_line(
+            horizontal_alignment, placed_text, height_of_previous_line,
+            height_of_current_line, line_spacing_of_previous_line,
+            line_spacing_of_current_line, width_of_current_line,
+            index_of_first_glyph_on_current_line, number_of_banked_lines);
 
         height_of_previous_line = height_of_current_line;
         height_of_current_line = 0;
@@ -72,63 +81,80 @@ void place_text(
         line_spacing_of_current_line = 0;
         width_of_current_line = 0;
         code_point_spacing_of_previous_character = -1;
-        index_of_first_glyph_on_current_line = placed_text->number_of_placed_glyphs;
+        index_of_first_glyph_on_current_line =
+            placed_text->number_of_placed_glyphs;
         number_of_banked_lines = 1;
       }
-    }
-    else
-    {
-      if (placed_text->number_of_placed_glyphs == placed_text->number_of_placeable_glyphs)
-      {
+    } else {
+      if (placed_text->number_of_placed_glyphs ==
+          placed_text->number_of_placeable_glyphs) {
         placed_text->number_of_placed_glyphs = -1;
         return;
       }
 
-      const text_font *const text_font = written_text.written_text_fonts[code_point_index];
+      const text_font *const text_font =
+          written_text.written_text_fonts[code_point_index];
 
-      if (code_point_spacing_of_previous_character != -1)
-      {
-        width_of_current_line += text_font->code_point_spacing > code_point_spacing_of_previous_character ? text_font->code_point_spacing : code_point_spacing_of_previous_character;
+      if (code_point_spacing_of_previous_character != -1) {
+        width_of_current_line +=
+            text_font->code_point_spacing >
+                    code_point_spacing_of_previous_character
+                ? text_font->code_point_spacing
+                : code_point_spacing_of_previous_character;
       }
 
-      placed_text->placed_glyph_columns[placed_text->number_of_placed_glyphs] = width_of_current_line + column;
-      placed_text->placed_glyph_rows[placed_text->number_of_placed_glyphs] = total_height + text_font->glyph_row_offsets[glyph_index] + row;
-      placed_text->placed_text_fonts[placed_text->number_of_placed_glyphs] = written_text.written_text_fonts[code_point_index];
-      placed_text->placed_glyph_indices[placed_text->number_of_placed_glyphs] = glyph_index;
-      placed_text->placed_glyph_opacities[placed_text->number_of_placed_glyphs] = written_text.written_opacities[code_point_index];
-      placed_text->placed_glyph_reds[placed_text->number_of_placed_glyphs] = written_text.written_reds[code_point_index];
-      placed_text->placed_glyph_greens[placed_text->number_of_placed_glyphs] = written_text.written_greens[code_point_index];
-      placed_text->placed_glyph_blues[placed_text->number_of_placed_glyphs] = written_text.written_blues[code_point_index];
+      placed_text->placed_glyph_columns[placed_text->number_of_placed_glyphs] =
+          width_of_current_line + column;
+      placed_text->placed_glyph_rows[placed_text->number_of_placed_glyphs] =
+          total_height + text_font->glyph_row_offsets[glyph_index] + row;
+      placed_text->placed_text_fonts[placed_text->number_of_placed_glyphs] =
+          written_text.written_text_fonts[code_point_index];
+      placed_text->placed_glyph_indices[placed_text->number_of_placed_glyphs] =
+          glyph_index;
+      placed_text
+          ->placed_glyph_opacities[placed_text->number_of_placed_glyphs] =
+          written_text.written_opacities[code_point_index];
+      placed_text->placed_glyph_reds[placed_text->number_of_placed_glyphs] =
+          written_text.written_reds[code_point_index];
+      placed_text->placed_glyph_greens[placed_text->number_of_placed_glyphs] =
+          written_text.written_greens[code_point_index];
+      placed_text->placed_glyph_blues[placed_text->number_of_placed_glyphs] =
+          written_text.written_blues[code_point_index];
       placed_text->number_of_placed_glyphs++;
 
       code_point_spacing_of_previous_character = text_font->code_point_spacing;
       width_of_current_line += text_font->glyph_widths[glyph_index];
 
-      if (text_font->line_height > height_of_current_line)
-      {
+      if (text_font->line_height > height_of_current_line) {
         height_of_current_line = text_font->line_height;
       }
 
-      if (text_font->line_spacing > line_spacing_of_current_line)
-      {
+      if (text_font->line_spacing > line_spacing_of_current_line) {
         line_spacing_of_current_line = text_font->line_spacing;
       }
     }
   }
 
-  end_line(horizontal_alignment, placed_text, height_of_previous_line, height_of_current_line, line_spacing_of_previous_line, line_spacing_of_current_line, width_of_current_line, index_of_first_glyph_on_current_line, number_of_banked_lines);
+  end_line(horizontal_alignment, placed_text, height_of_previous_line,
+           height_of_current_line, line_spacing_of_previous_line,
+           line_spacing_of_current_line, width_of_current_line,
+           index_of_first_glyph_on_current_line, number_of_banked_lines);
 
-  if (vertical_alignment != BELOW_TEXT_ANCHOR)
-  {
-    total_height += (line_spacing_of_previous_line + height_of_previous_line) * number_of_banked_lines;
+  if (vertical_alignment != BELOW_TEXT_ANCHOR) {
+    if (number_of_banked_lines == 0) {
+      total_height = height_of_current_line;
+    } else {
+      total_height +=
+          (line_spacing_of_previous_line + height_of_previous_line) *
+          number_of_banked_lines;
+    }
 
-    if (vertical_alignment == CENTERED_ON_TEXT_ANCHOR)
-    {
+    if (vertical_alignment == CENTERED_ON_TEXT_ANCHOR) {
       total_height /= 2;
     }
 
-    for (int glyph_index = 0; glyph_index < placed_text->number_of_placed_glyphs; glyph_index++)
-    {
+    for (int glyph_index = 0;
+         glyph_index < placed_text->number_of_placed_glyphs; glyph_index++) {
       placed_text->placed_glyph_rows[glyph_index] -= total_height;
     }
   }
---
+++ tests/place_single_line_above/main.c
@@ -0,0 +1,181 @@
+#include "../../src/place_text.h"
+#include "../../src/placed_text.h"
+#include "../../src/start_text.h"
+#include "../../src/text_font.h"
+#include "../../src/write_text_code_point.h"
+#include "../../src/write_text_new_line.h"
+#include "../../src/written_text.h"
+#include <stdint.h>
+#include <stdio.h>
+
+static int exit_code = 0;
+
+static void assert_int(const char *const description, const int expected,
+                       const int actual) {
+  if (expected != actual) {
+    fprintf(stderr, "Expected %d %s, actual %d.\n", expected, description,
+            actual);
+    exit_code = 1;
+  }
+}
+
+static void assert_float(const char *const description, const float expected,
+                         const float actual) {
+  if (expected != actual) {
+    fprintf(stderr, "Expected %f %s, actual %f.\n", expected, description,
+            actual);
+    exit_code = 1;
+  }
+}
+
+static void assert_pointer(const char *const description,
+                           const void *const expected,
+                           const void *const actual) {
+  if (expected != actual) {
+    fprintf(stderr, "Expected %p %s, actual %p.\n", expected, description,
+            actual);
+    exit_code = 1;
+  }
+}
+
+int main(const int argc, const char *const *const argv) {
+  (void)(argc);
+  (void)(argv);
+
+  const text_font *written_text_fonts[7];
+  int written_glyph_indices[7];
+  float written_opacities[7];
+  float written_reds[7];
+  float written_greens[7];
+  float written_blues[7];
+  written_text written = start_text(
+      7, written_text_fonts, written_glyph_indices, written_opacities,
+      written_reds, written_greens, written_blues);
+
+  const int glyph_widths_a[] = {23, 18, 19, 25};
+  const uint32_t glyph_code_points_a[] = {505255796, 204668171, 358868195,
+                                          402862483};
+  const int glyph_row_offsets_a[] = {5, -7, 8, 2};
+
+  const text_font font_a = {.code_point_spacing = 5,
+                            .line_spacing = 3,
+                            .line_height = 12,
+                            .glyph_widths = glyph_widths_a,
+                            .glyph_code_points = glyph_code_points_a,
+                            .glyph_row_offsets = glyph_row_offsets_a,
+                            .number_of_glyphs = 4};
+
+  const int glyph_widths_b[] = {50, 49, 37};
+  const uint32_t glyph_code_points_b[] = {204668171, 505255796, 37498374};
+  const int glyph_row_offsets_b[] = {3, 9, -2};
+
+  const text_font font_b = {.code_point_spacing = 41,
+                            .line_spacing = 25,
+                            .line_height = 17,
+                            .glyph_widths = glyph_widths_b,
+                            .glyph_code_points = glyph_code_points_b,
+                            .glyph_row_offsets = glyph_row_offsets_b,
+                            .number_of_glyphs = 3};
+
+  write_text_code_point(204668171, &font_a, 0.1564262168f, 0.0437286089f,
+                        0.2452098142f, 0.4578030974f, &written);
+  write_text_code_point(505255796, &font_b, 0.3664873978f, 0.8843461135f,
+                        0.0204608919f, 0.8177569126f, &written);
+  write_text_code_point(204668171, &font_b, 0.5377511140f, 0.6091213575f,
+                        0.3598658273f, 0.4994471018f, &written);
+  write_text_code_point(37498374, &font_b, 0.1292127857f, 0.3058116156f,
+                        0.5600688226f, 0.6124764171f, &written);
+  write_text_code_point(505255796, &font_a, 0.0826451742f, 0.7329012169f,
+                        0.3891597672f, 0.3621127009f, &written);
+  write_text_code_point(204668171, &font_a, 0.6479530738f, 0.2324556519f,
+                        0.6618752571f, 0.2592586932f, &written);
+
+  const text_font *placed_text_fonts[6];
+  int placed_glyph_indices[6];
+  int placed_glyph_rows[6];
+  int placed_glyph_columns[6];
+  float placed_glyph_opacities[6];
+  float placed_glyph_reds[6];
+  float placed_glyph_greens[6];
+  float placed_glyph_blues[6];
+  placed_text placed = {
+      .number_of_placeable_glyphs = 6,
+      .placed_text_fonts = placed_text_fonts,
+      .placed_glyph_indices = placed_glyph_indices,
+      .placed_glyph_rows = placed_glyph_rows,
+      .placed_glyph_columns = placed_glyph_columns,
+      .placed_glyph_opacities = placed_glyph_opacities,
+      .placed_glyph_reds = placed_glyph_reds,
+      .placed_glyph_greens = placed_glyph_greens,
+      .placed_glyph_blues = placed_glyph_blues,
+  };
+
+  place_text(written, ABOVE_TEXT_ANCHOR, RIGHT_OF_TEXT_ANCHOR, 1000, -2000,
+             &placed);
+
+  assert_int("placed glyphs", 6, placed.number_of_placed_glyphs);
+
+  assert_pointer("placed text font a", &font_a, placed_text_fonts[0]);
+  assert_pointer("placed text font b", &font_b, placed_text_fonts[1]);
+  assert_pointer("placed text font c", &font_b, placed_text_fonts[2]);
+  assert_pointer("placed text font d", &font_b, placed_text_fonts[3]);
+  assert_pointer("placed text font e", &font_a, placed_text_fonts[4]);
+  assert_pointer("placed text font f", &font_a, placed_text_fonts[5]);
+
+  assert_int("placed glyph index a", 1, placed_glyph_indices[0]);
+  assert_int("placed glyph index b", 1, placed_glyph_indices[1]);
+  assert_int("placed glyph index c", 0, placed_glyph_indices[2]);
+  assert_int("placed glyph index d", 2, placed_glyph_indices[3]);
+  assert_int("placed glyph index e", 0, placed_glyph_indices[4]);
+  assert_int("placed glyph index f", 1, placed_glyph_indices[5]);
+
+  assert_int("placed glyph row a", 981, placed_glyph_rows[0]);
+  assert_int("placed glyph row b", 992, placed_glyph_rows[1]);
+  assert_int("placed glyph row c", 986, placed_glyph_rows[2]);
+  assert_int("placed glyph row d", 981, placed_glyph_rows[3]);
+  assert_int("placed glyph row e", 993, placed_glyph_rows[4]);
+  assert_int("placed glyph row f", 981, placed_glyph_rows[5]);
+
+  assert_int("placed glyph column a", -2000, placed_glyph_columns[0]);
+  assert_int("placed glyph column b", -1941, placed_glyph_columns[1]);
+  assert_int("placed glyph column c", -1851, placed_glyph_columns[2]);
+  assert_int("placed glyph column d", -1760, placed_glyph_columns[3]);
+  assert_int("placed glyph column e", -1682, placed_glyph_columns[4]);
+  assert_int("placed glyph column f", -1654, placed_glyph_columns[5]);
+
+  assert_float("placed glyph opacity a", 0.1564262168f,
+               placed_glyph_opacities[0]);
+  assert_float("placed glyph opacity b", 0.3664873978f,
+               placed_glyph_opacities[1]);
+  assert_float("placed glyph opacity c", 0.5377511140f,
+               placed_glyph_opacities[2]);
+  assert_float("placed glyph opacity d", 0.1292127857f,
+               placed_glyph_opacities[3]);
+  assert_float("placed glyph opacity e", 0.0826451742f,
+               placed_glyph_opacities[4]);
+  assert_float("placed glyph opacity f", 0.6479530738f,
+               placed_glyph_opacities[5]);
+
+  assert_float("placed glyph red a", 0.0437286089f, placed_glyph_reds[0]);
+  assert_float("placed glyph red b", 0.8843461135f, placed_glyph_reds[1]);
+  assert_float("placed glyph red c", 0.6091213575f, placed_glyph_reds[2]);
+  assert_float("placed glyph red d", 0.3058116156f, placed_glyph_reds[3]);
+  assert_float("placed glyph red e", 0.7329012169f, placed_glyph_reds[4]);
+  assert_float("placed glyph red f", 0.2324556519f, placed_glyph_reds[5]);
+
+  assert_float("placed glyph green a", 0.2452098142f, placed_glyph_greens[0]);
+  assert_float("placed glyph green b", 0.0204608919f, placed_glyph_greens[1]);
+  assert_float("placed glyph green c", 0.3598658273f, placed_glyph_greens[2]);
+  assert_float("placed glyph green d", 0.5600688226f, placed_glyph_greens[3]);
+  assert_float("placed glyph green e", 0.3891597672f, placed_glyph_greens[4]);
+  assert_float("placed glyph green f", 0.6618752571f, placed_glyph_greens[5]);
+
+  assert_float("placed glyph blue a", 0.4578030974f, placed_glyph_blues[0]);
+  assert_float("placed glyph blue b", 0.8177569126f, placed_glyph_blues[1]);
+  assert_float("placed glyph blue c", 0.4994471018f, placed_glyph_blues[2]);
+  assert_float("placed glyph blue d", 0.6124764171f, placed_glyph_blues[3]);
+  assert_float("placed glyph blue e", 0.3621127009f, placed_glyph_blues[4]);
+  assert_float("placed glyph blue f", 0.2592586932f, placed_glyph_blues[5]);
+
+  return exit_code;
+}
